80ad11787be828b5bfb8d6e8f11885be9f43d7d1
SomilaTshapu1 <82814273+SomilaTshapu1@users.noreply.github.com>
2021-06-10 16:05:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Please Fill in the details!");
+            Console.Write("Enter your Name");
+
+            string Firstname;
+            Firstname = Console.ReadLine();
+
+            Console.Write("Enter your Surname");
+
+            string Surname;
+            Surname = Console.ReadLine();
+           
+
+            Console.Write("Enter your Student Number");
+            int StudentNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your Course Code");
+
+            string Course;
+            Course = Console.ReadLine();
+
+            Console.Write("Enter your Cellphone Number");
+            int Cellphone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Hello," + Firstname + "" + Surname);
+            Console.WriteLine("Your Contact Number is," + Cellphone );
+            Console.WriteLine("Your Student Number is," + StudentNumber);
+            Console.WriteLine("Your Course is," + Course);
+
+        }
+    }
+}
